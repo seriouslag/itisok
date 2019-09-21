2d142883ac19d1e2a0e356b4631406a5d2ab4614
seriouslag <Landon.Gavin@gmail.com>
2019-09-21 14:39:44 -0400

    add label

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,12 +12,18 @@
         You're not alone.
       </div>
       <div class="item">
-        <a
-          class="link "
-          href="tel:1-800-273-8255"
-        >
-          1-800-273-8255
-        </a>
+        <div>
+          <a
+            class="link"
+            href="tel:1-800-273-8255"
+            aria-labelledby="hotline"
+          >
+            1-800-273-8255
+          </a>
+        </div>
+        <div id="hotline">
+          National Suicide Prevention Lifeline
+        </div>
       </div>
     </footer>
 </template>
