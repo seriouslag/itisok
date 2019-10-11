0bf0a551e072b6c618d9358c60b9d6c0e2233cdf
seriouslag <Landon.Gavin@gmail.com>
2019-10-10 23:22:13 -0400

    move css

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -4,19 +4,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Overlay extends Vue {}
 </script>
-
-<style lang="scss" scoped>
-  .overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: radial-gradient(rgba(17,17,17, 0.1) 10%, rgba(17,17,17, 0.2) 30%, rgba(17,17,17, 0.3) 70%);
-  }
-</style>
